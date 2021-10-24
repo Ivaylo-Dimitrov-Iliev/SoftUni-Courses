d4c465f4c96c17dee7b1a4d2c2e534938b9ab58f
Iliev <ivayloiliev0000@gmail.com>
2021-10-24 08:56:10 +0300

    Added python code files from python basics module in SoftUni

---
+++ 01_programing_basics_with_python_march_2021/02_exercise/06_while_loops_exercise/04_walking.py
@@ -0,0 +1,28 @@
+# input
+
+command = input()
+
+# variable
+
+count_of_steps = 0
+
+# loop
+
+while command != 'Going home':
+    steps = int(command)
+    count_of_steps += steps
+    if count_of_steps >= 10000:
+        break
+    command = input()
+
+else:
+    steps_to_home = int(input())
+    count_of_steps += steps_to_home
+
+if count_of_steps >= 10000:
+    surplus_steps = count_of_steps - 10000
+    print('Goal reached! Good job!')
+    print(f'{surplus_steps} steps over the goal!')
+else:
+    needed_steps = 10000 - count_of_steps
+    print(f'{needed_steps} more steps to reach goal.')
